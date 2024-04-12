8a2b75c8c12e4e93b45d9bdd4354224ebbe2ffdc
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-12 10:34:00 -0300

    [ :sparkles: ] | create: check - health (app::health)

--- pkg/health/checker.go
+++ pkg/health/checker.go
@@ -2,6 +2,7 @@ package health
 
 import (
 	"errors"
+	"net"
 	"time"
 
 	"github.com/Samuel-Ricardo/load_balancer/pkg/domain"
@@ -37,4 +38,30 @@ func (hc *HealthChecker) start() {
 			}
 		}
 	}
+}
+
+func checkHealth(server *domain.Server) {
+  
+  _, err := net.DialTimeout("tcp", server.Url.Host, time.Second*5)
+
+  if err != nil {
+
+    log.Errorf(Could not connect to %s: %s, error: %s", server.Url.Host, server.Url.Path, err.Error())
+    old := server.SetLiveness(false)
+  
+    if old {
+      log.Warnf("Server %s is dead", server.Url.Host)
+      log.Warnf("Transitioning server '%s' from Live to Unavailable state", server.Url.Host)
+    }
+
+    return
+  }
+
+  old := server.SetLiveness(true)
+  
+  if old {
+    log.Warnf("Server %s is alive", server.Url.Host)
+    log.Warnf("Transitioning server '%s' from Unavailable to Live state", server.Url.Host)
+  }
+
 }
