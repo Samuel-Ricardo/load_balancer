e7962bbc18faf3755541e6da7a0aab2b1e4e860e
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-12 10:52:05 -0300

    [ :sparkles: ] | setup: supported strategies (app::strategy)

--- pkg/strategy/api.go
+++ pkg/strategy/api.go
@@ -1,7 +1,15 @@
 package strategy
 
+import "github.com/Samuel-Ricardo/load_balancer/pkg/domain"
+
 const (
 	kRoundRobin         = "RoundRobin"
 	kWeightedRoundRobin = "WeightedRoundRobin"
 	kUnknown            = "Unknown"
 )
+
+type BalancingStrategy interface {
+	Next([]*domain.Server) (*domain.Server, error)
+}
+
+var stratefies map[string]func() BalancingStrategy
