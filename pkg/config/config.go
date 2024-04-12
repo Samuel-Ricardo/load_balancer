7893b1327cfe7038b6bc9976fb833c2d4be460b0
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-12 16:19:20 -0300

    [ :gear: ] | setup: config - server (app::config)

---
+++ pkg/config/config.go
@@ -0,0 +1,19 @@
+package config
+
+import (
+	"github.com/Samuel-Ricardo/load_balancer/pkg/domain"
+	"github.com/Samuel-Ricardo/load_balancer/pkg/health"
+	"github.com/Samuel-Ricardo/load_balancer/pkg/strategy"
+)
+
+type config struct {
+	Services []domain.Service `yaml:"services"`
+	Strategy string           `yaml:"strategy"`
+}
+
+type ServerList struct {
+	Servers  []*domain.Server
+	Name     string
+	Strategy strategy.BalancingStrategy
+	Hc       *health.HealthChecker
+}
