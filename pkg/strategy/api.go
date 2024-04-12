ec2d3182369494c3bafd2abc14043c081838722b
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-12 11:11:57 -0300

    [ :sparkles: ] | implements: strategy - round robin (app::strategy)

--- pkg/strategy/api.go
+++ pkg/strategy/api.go
@@ -1,9 +1,13 @@
 package strategy
 
 import (
+	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/Samuel-Ricardo/load_balancer/pkg/domain"
+	"github.com/pingcap/log"
+	"golang.org/x/crypto/openpgp/errors"
 )
 
 const (
@@ -37,9 +41,32 @@ type RoundRobin struct {
 	current int
 }
 
-// Next implements BalancingStrategy.
-func (r *RoundRobin) Next([]*domain.Server) (*domain.Server, error) {
-	panic("unimplemented")
+// NOTE: implements Round Robin algorithm for BalancingStrategy interface
+func (r *RoundRobin) Next(servers []*domain.Server) (*domain.Server, error) {
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	seen := 0
+	var picked *domain.Server
+
+	for seen < len(servers) {
+		picked = servers[r.current]
+		r.current = (r.current + 1) % len(servers)
+
+		if picked.IsAlive() {
+			break
+		}
+
+		seen++
+	}
+
+	if picked == nil || seen == len(servers) {
+		log.Error("All servers are dead")
+		return nil, errors.New(fmt.Sprintf("Checked all the '%d' servers, none of them are alive", seen))
+	}
+
+	log.Infof("Strategy picked server: %s", picked.Url.Host)
+	return picked, nil
 }
 
 // INFO: RoundRobin with weight
