bdf07499739fdc875fa3f41aaddfd39288c0cebe
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-12 11:04:59 -0300

    [ :sparkles: ] | setup: init - strategies (app::strategy)

--- pkg/strategy/api.go
+++ pkg/strategy/api.go
@@ -18,10 +18,28 @@ type BalancingStrategy interface {
 
 var strategies map[string]func() BalancingStrategy
 
+func init() {
+	strategies[kRoundRobin] = func() BalancingStrategy {
+		return &RoundRobin{
+			mu:      sync.Mutex{},
+			current: 0,
+		}
+	}
+
+	strategies[kWeightedRoundRobin] = func() BalancingStrategy {
+		return &WeightedRoundRobin{mu: sync.Mutex{}}
+	}
+}
+
 // INFO: Based simple incrementable counter, just go to next server, current+1
 type RoundRobin struct {
 	mu      sync.Mutex
 	current int
+}
+
+// Next implements BalancingStrategy.
+func (r *RoundRobin) Next([]*domain.Server) (*domain.Server, error) {
+	panic("unimplemented")
 }
 
 // INFO: RoundRobin with weight
@@ -29,4 +47,9 @@ type WeightedRoundRobin struct {
 	count   []int
 	current int
 	mu      sync.Mutex
+}
+
+// Next implements BalancingStrategy.
+func (w *WeightedRoundRobin) Next([]*domain.Server) (*domain.Server, error) {
+	panic("unimplemented")
 }
